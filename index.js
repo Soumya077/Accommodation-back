23ccdc224024ae4353fecaf6c769c18ad7373c40
Soumya <soumyajain1354@gmail.com>
2024-09-21 23:45:14 +0530

    BLAH

--- index.js
+++ index.js
@@ -33,6 +33,10 @@ mongoose.connect(process.env.MONGO_URL);
 console.log(process.env.MONGO_URL);
 console.log(process.env.JWT_SECRET);
 
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
 app.get('/api/test' , (req,res) => {
     res.json('test ok');
 });
